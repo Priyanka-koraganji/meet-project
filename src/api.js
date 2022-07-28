e43fce639240106c4a60b9497fbda0bbf0285fe3
Priyanka-koraganji <51729374+Priyanka-koraganji@users.noreply.github.com>
2022-07-27 17:57:47 -0700

    app caching

--- src/api.js
+++ src/api.js
@@ -30,6 +30,11 @@ export const getEvents = async () => {
         NProgress.done();
         return mockData;
     }
+    if (!navigator.onLine) {
+        const data = localStorage.getItem("lastEvents");
+        NProgress.done();
+        return data ? JSON.parse(events).events : [];;
+    }
     const token = await getAccessToken();
     if (token) {
         removeQuery();
@@ -40,6 +45,7 @@ export const getEvents = async () => {
             localStorage.setItem("lastEvents", JSON.stringify(result.data));
             localStorage.setItem("locations", JSON.stringify(locations));
         }
+
         NProgress.done();
         return result.data.events;
     }
