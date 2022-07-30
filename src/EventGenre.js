59b7df854ab5b68444b0e3dad4195b6bdcd841aa
Priyanka-koraganji <51729374+Priyanka-koraganji@users.noreply.github.com>
2022-07-30 16:48:46 -0700

    fixes

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -20,7 +20,7 @@ const EventGenre = ({ events }) => {
         // eslint-disable-next-line
     }, [events]);
     return (
-        <ResponsiveContainer>
+        <ResponsiveContainer height={400}>
             <PieChart width={400} height={400}>
                 <Pie
                     data={data}
